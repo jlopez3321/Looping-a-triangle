2467fba85b15458ed0f07726c1d5ac2f3be01846
Juan Antonio López <juanantonio8686@outlook.com>
2023-03-16 12:37:47 +0100

    code

---
+++ script.js
@@ -0,0 +1,10 @@
+// Write a loop that makes seven calls to console.log to output a triangle:
+
+// Triangle piece
+let hash = "#";
+
+// Add pieces to build the triangle
+for (let i = 0; i < 7; i++) {
+    console.log(hash);
+    hash += "#";
+}
